041e19ce7fb40c3fb90b7449a01d43b28160dc10
LidorLevi123 <lidorlevi728@gmail.com>
2024-08-26 17:42:54 +0300

    add link to google maps

--- js/country.controller.js
+++ js/country.controller.js
@@ -11,21 +11,27 @@ function onSearch(ev) {
 }
 
 function renderCountry(country) {
-    console.log('country:', country)
+    const { common: countryName } = country.name
+    const [lat, lng] = country.latlng
+
     const strHTML = `
-        <h2 class="name">${country.name.common}</h2>
+        <h2 class="name">${countryName}</h2>
         <img class="flag-img" src="${country.flags.png}" alt="">
         <p class="flag-description">${country.flags.alt}</p>
         <p class="population">Population: ${country.population}</p>
-        <p class="area">Area: ${country.area}</p>`
+        <p class="area">Area: ${country.area}</p>
+        <a href="https://www.google.com/maps/place/${countryName}/@${lat},${lng},7z/">
+            Click to view ${countryName} on Google Maps!
+        </a>`
 
+    console.log('country:', country)
     const elCountry = document.querySelector('.country-preview')
     elCountry.innerHTML = strHTML
     setTimeout(window.scrollTo({ top: document.body.scrollHeight }), 500)
 }
 
 function onRemoveCache() {
-    if(!confirm('Are you sure?')) return
+    if (!confirm('Are you sure?')) return
     removeCache()
 }
 
